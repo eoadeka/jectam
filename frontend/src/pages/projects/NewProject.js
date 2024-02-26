15f95ecfce72ae90f693e5e3f558473b68e61a09
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-02-26 00:11:22 +0000

    feat: Add NewProjectForm components

--- frontend/src/pages/projects/NewProject.js
+++ frontend/src/pages/projects/NewProject.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Container from '../../components/layout/Container';
+import NewProjectForm from '../../components/forms/NewProjectForm';
 
 
 const NewProject = () => {
   return (
-    <div>
-        <Container>
-            <h1>New Project</h1>
-        </Container>
-    </div>
+    <Container>
+        <h1>New Project</h1>
+        {/* <p>Project name e.g AutoTasker </p>
+        <p>Project description e.g A system that automates task scheduling and assignment based on priority and resource availability.</p>
+        <p>project specifics eg budget (£400), documentation-heavy? or not, team-size, time-bound, adaptable to change</p>
+
+        <span>Best fit methodolology:<small> Prince2</small></span>
+        <p>carry on? or choose another methodolology</p> */}
+
+       
+          <NewProjectForm />
+        
+    </Container>
   );
 }
 
