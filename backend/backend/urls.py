164958e34936a57bc17738871c5c2db2e45c2202
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-02-28 23:59:26 +0000

    feat: Add urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('api.urls')),
     path('notifications/', include('notifications.urls')),
     path('projects/', include('projects.urls')),
