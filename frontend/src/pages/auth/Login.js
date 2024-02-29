1d01e8063f4d74dbbd4d8577a184465108184e92
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-02-29 23:13:54 +0000

    feat: Import LoginForm component

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from '../../components/layout/Container';
 import Button from '../../components/buttons/Button';
+import LoginForm from '../../components/forms/auth/login/LoginForm';
 
 // https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
 // https://www.copycat.dev/blog/material-ui-form/
@@ -14,19 +15,7 @@ const Login = () => {
         <h1>Login</h1>
         <div>
          <h2> Welcome back!</h2>
-         <form>
-            <input placeholder='Work Email' /><br></br>
-            <input placeholder='Password' /><br></br>
-            <p>Forgot Password?</p>
-
-            <Button>Continue</Button>
-
-            <hr></hr>
-            <Button>Login with Google</Button><br></br><br></br>
-            <Button>Login with Apple</Button><br></br>
-
-            <small>Haven't signed up yet? <a href='/sign-up' style={{textDecoration: "underline"}}>Sign Up</a></small><br></br>
-         </form>
+         <LoginForm />
         </div>
     </Container>
   );
