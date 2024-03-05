0a485e99aa289be914da88f66ffd970ba49bae14
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-05 23:58:14 +0000

    feat: Add Gantt chart element

---
+++ frontend/src/components/projects/ProjectGanttChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ProjectGanttChart = () => {
+  return (
+    <div>
+        <h1>Project Gantt Chart</h1>
+    </div>
+  );
+}
+
+export default ProjectGanttChart;
