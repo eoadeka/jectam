abc34b2580a1cb6c02834356a839a675d899f0d9
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-08 23:38:13 +0000

    feat: Connect to API for login

--- frontend/src/components/forms/auth/login/LoginForm.js
+++ frontend/src/components/forms/auth/login/LoginForm.js
@@ -1,19 +1,59 @@
+import { useState } from "react";
 import Button from "../../../buttons/Button"
+import { Input } from "../../FormElement";
+import axios from "axios";
 
 const LoginForm = () => {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    // Create submit method
+    const submit = async e => {
+        e.preventDefault();
+
+        const user = {
+            email: email,
+            password: password
+        };
+
+        // Create POST request
+        const { data } = await axios.post('http://localhost:8000/token/',
+        user, {headers: {'Content-Type': 'application/json'}, withCredentials: true});
+
+        // Initialise the access & refresh token in localstorage
+        localStorage.clear();
+        localStorage.setItem('access_token', data.access);
+        localStorage.setItem('refresh_token', data.refresh);
+
+        axios.defaults.headers.common['Authorization'] =  `Bearer ${data['access']}`;
+        window.location.href = '/dashboard'
+    }
     return (
-        <form>
-            <input placeholder='Work Email' /><br></br>
-            <input placeholder='Password' /><br></br>
+        <form onSubmit={submit}>
+            <Input 
+                placeholder='Work Email'
+                type="email"
+                value={email}
+                required 
+                onChange={e => setEmail(e.target.value)}
+            /><br></br>
+            <Input 
+                placeholder='Password'
+                type="password"
+                value={password}
+                required 
+                onChange={e => setPassword(e.target.value)}
+             /><br></br>
             <p>Forgot Password?</p>
 
-            <Button>Continue</Button>
+            <Button type="submit">Continue</Button>
+            <br></br>
 
-            <hr></hr>
+            {/* <hr></hr> */}
             <Button>Login with Google</Button><br></br><br></br>
             <Button>Login with Apple</Button><br></br>
 
-            <small>Haven't signed up yet? <a href='/sign-up' style={{textDecoration: "underline"}}>Sign Up</a></small><br></br>
+            {/* <small>Haven't signed up yet? <a href='/sign-up' style={{textDecoration: "underline"}}>Sign Up</a></small><br></br> */}
          </form>
     )
 }
