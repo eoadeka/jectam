f34ccaa94e4401e72e1c011f0b4aa5efad05c5ee
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-10 23:56:44 +0000

    feat: Add handleSubmit for auth form

--- frontend/src/components/forms/auth/AuthForm.js
+++ frontend/src/components/forms/auth/AuthForm.js
@@ -31,9 +31,56 @@ const AuthForm = () => {
         }
       };
 
-    const handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault()
-        console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
+        let formData = new FormData()
+        formData.append('profile_picture', data.profile_picture)
+        formData.append('role', data.role)
+        formData.append('email', data.email)
+        formData.append('password1', data.password1)
+        formData.append('password2', data.password2)
+        formData.append('accepted_terms', data.accepted_terms)
+        formData.append('phone_number', data.phone_number)
+        formData.append('otp', data.otp)
+        formData.append('first_name', data.first_name)
+        formData.append('last_name', data.last_name)
+        formData.append('birth_date', data.birth_date)
+        formData.append('gender', data.gender)
+
+        const user = {
+          role: data.role,
+          email: data.email,
+          password1: data.password1,
+          password2: data.password2,
+          accepted_terms: data.accepted_terms,
+          phone_number: data.phone_number,
+          otp: data.otp,
+          first_name: data.first_name,
+          last_name: data.last_name,
+          birth_date: data.birth_date,
+          gender: data.gender,
+          profile_picture: data.profile_picture
+        }
+
+        console.log(user)
+        await fetch('http://localhost:8000/accounts/dj-rest-auth/registration/', {
+            method: 'POST',
+            // headers: {
+            //   Accept: 'application/json',
+            //   'Content-Type': 'multipart/form-data',
+            // },
+            body: formData
+          })
+        .then(res => {
+          res.json()
+        })
+        .catch(error => {
+          console.error(error);
+        });
+        window.location.href = '/login'
+
+        
     }
 
 
