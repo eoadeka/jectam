68638f16d63b49da751b95e2c218df94f010910f
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-12 23:54:36 +0000

    feat: add logout action

--- frontend/src/pages/auth/Logout.js
+++ frontend/src/pages/auth/Logout.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import Container from '../../components/layout/Container';
-
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 const Logout = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const {data} = await  
+          axios.post(
+            'http://localhost:8000/accounts/logout/', 
+          { refresh_token:localStorage.getItem('refresh_token')} ,
+          {headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`
+          },  
+          withCredentials: true});
+        console.log('logout', data)
+        localStorage.clear();
+        axios.defaults.headers.common['Authorization'] = null;
+        window.location.href = '/login'
+      } catch (e) {
+          console.log('logout not working', e)
+        }
+        })();
+    }, []);
   return (
-    <Container>
-        <h1>Logout</h1>
-        
-    </Container>
+    <></>
   );
 }
 
