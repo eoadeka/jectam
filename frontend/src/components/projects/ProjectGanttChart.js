df5dd5b7ea7d52c22b3b2c0cfcbdf4072e91e279
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-13 23:49:32 +0000

    chore: edit tasks to match api

--- frontend/src/components/projects/ProjectGanttChart.js
+++ frontend/src/components/projects/ProjectGanttChart.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
 
 
-const ProjectGanttChart = () => {
-  return (
-    <div>
-        <h1>Project Gantt Chart</h1>
-    </div>
-  );
+const ProjectGanttChart = ({tasks}) => {
+    const taskss = [
+        {
+            start: new Date(2020, 1, 1),
+            end: new Date(tasks[0].due_date),
+            name: tasks[0].title,
+            id: tasks.task_id,
+            type: 'task',
+            progress: 45,
+            isDisabled: true,
+            styles: { progressColor: 'gainsboro', progressSelectedColor: '#ff9e0d', textColor: 'black' },
+        },
+        // Add other tasks here
+    ];
+
+    // console.log(tasks)
+
+    const view = [ "Hour", "Quarter Day", "Half Day", "Day"]
+  
+
+    return (
+        <Gantt
+            tasks={taskss}
+            viewMode={view}
+            // onDateChange={onTaskChange}
+            // onTaskDelete={onTaskDelete}
+            // onProgressChange={onProgressChange}
+            // onDoubleClick={onDblClick}
+            // onClick={() = {console.log("me")}}
+            // onClick={onClick}
+      />
+    // <Gantt tasks={tasks} />
+    );
 }
 
 export default ProjectGanttChart;
