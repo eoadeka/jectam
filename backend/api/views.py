3b1b6e89289c1fb33f50524fcfbbd9bacf6d5184
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-22 23:29:43 +0000

    fix: Add permission_class to solve Unauthorised error

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def hello_world(request):
     return Response({'message': 'Hello, world there! the world is shining'})
+
 
 class HomeView(APIView):
      
