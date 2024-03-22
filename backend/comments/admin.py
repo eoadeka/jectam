5373f3051070a40ed82f2965ae5d38102b9ed20c
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-22 23:32:18 +0000

    feat: Register Comments in admin site

--- backend/comments/admin.py
+++ backend/comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('commenter','task', 'comment')
+
+
+admin.site.register(Comment, CommentsAdmin)
