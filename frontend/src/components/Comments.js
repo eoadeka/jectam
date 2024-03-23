8333c2d0e47ce8ff942ba1ef724ec62f3df6d0f3
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-23 23:53:58 +0000

    fix: Fix user details error for commenter

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,12 +1,48 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import moment from 'moment';
 
-const Comments = () => {
+
+const Comments = ({comment, commenterFirstName, commenterLastName}) => {
+
+
+    const timeAgoString = () => {
+        const timeElapsed = moment(comment.timeStamp).fromNow();
+        
+        // Split the time elapsed string to extract numeric value and unit
+        const [value, unit] = timeElapsed.split(' ');
+    
+        switch (unit) {
+        case 'seconds':
+            return `${value}s`;
+          case 'minutes':
+            return value === 'a' ? '1m' : `${value}m`;
+          case 'hours':
+            return `${value}h`;
+          case 'days':
+            return `${value}d`;
+          case 'weeks':
+            return `${value}w`;
+          case 'months':
+            return `${value}mo`;
+          default:
+            return timeElapsed;
+        }
+      };
+
     return (
-        <div>
+        <div style={{ width:"100%"}}>
             <div>
-                {/* comment 1 */}
-                
+                <AvatarGroup sx={{display:"inline-block", width: "15%",  verticalAlign:"middle"}}>
+                    <Avatar alt={commenterFirstName}  src={comment.commenter_profile_pic} sx={{ width: 24, height: 24 }} />
+                </AvatarGroup>
+                <small style={{display:"inline-block", width: "65%",  verticalAlign:"middle", fontSize: "0.95em"}}>{commenterFirstName} {commenterLastName}</small>
+                <small style={{display:"inline-block", width: "20%",  verticalAlign:"middle", textAlign:"right", opacity:"0.6", fontSize:"0.8em"}}>{timeAgoString()}</small>
             </div>
+            <p>{comment.comment}</p>
+            <hr></hr>
+            {/* {comment.read === false && (<span>New</span>)} */}
         </div>
     )
 }
