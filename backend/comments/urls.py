41808901c7d61eeab22a8949795acbbcaca962c8
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-03-26 23:48:29 +0000

    fix: Fix paths to comment views

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -6,6 +6,7 @@ from .views import *
 urlpatterns = [
 
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
-    path('comments/<str:task>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment-detail'),
+    path('comments/<str:task>/comments', TaskCommentsAPIView.as_view(), name='comment-detail'),
+    path('comments/<str:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment-detail'),
     path('comments/details/<str:comment_id>/', CommentDetail.as_view(), name='comment-detail'),
 ] 
